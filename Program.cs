de233539ebe509d709d954416571cdf0d215cdc6
Staxxx <sersersin@gmail.com>
2022-07-31 23:38:48 +0300

    добавили 13 решение, тестируем

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿//  Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет
+
+
+//обьявляем глабальную переменную - массив из трех элементов
+char[] mas = new char[3];
+int third;
+
+
+//заполняем массив с полученной строки из консоли
+void make_array()
+{
+            Console.WriteLine("Введите массив:");       
+            string s = Console.ReadLine();
+            third = s.Length;
+
+            char[] entered_mas = new char[s.Length];
+            //разбиваем полученную строку на массив 
+            for (int i = 0; i < s.Length; i++)
+            {
+                entered_mas[i] = s[i];
+                //заполняем глобально созданный массив
+                mas[i] = entered_mas[i];
+            }
+
+            Console.ReadLine();
+
+}
+
+// выводим третий элемент массива 
+void third_item(char[] mas)
+{
+   Console.WriteLine("третий элемет:");
+   Console.WriteLine(mas[2]);
+}
+
+void third_element()
+{
+    if (third < 3)
+    Console.WriteLine("Третьего элемента нет");
+    else
+   third_item(mas); 
+}
+
+make_array();
+
