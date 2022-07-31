7e238527884ee24c2382c0a3c59dcf9685dcb4c1
Staxxx <sersersin@gmail.com>
2022-07-31 23:24:56 +0300

    работает!

--- 10/Program.cs
+++ 10/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//обьявляем глабальную переменную - массив из трех элементов
+char[] mas = new char[3];
+
+//заполняем массив с полученной строки из консоли
+void make_array()
+{
+            Console.WriteLine("Введите массив:");       
+            string s = Console.ReadLine();
+
+            char[] entered_mas = new char[s.Length];
+            //разбиваем полученную строку на массив 
+            for (int i = 0; i < s.Length; i++)
+            {
+                entered_mas[i] = s[i];
+                //заполняем глобально созданный массив
+                mas[i] = entered_mas[i];
+            }
+
+            Console.ReadLine();
+
+}
+
+// выводим второй элемент массива 
+void second_item(char[] mas)
+{
+   Console.WriteLine("Второй элемет:");
+   Console.WriteLine(mas[1]);
+}
+//выполняем
+make_array();
+second_item(mas);
